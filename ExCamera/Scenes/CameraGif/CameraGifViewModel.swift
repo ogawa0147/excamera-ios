859410a1fdb70d412fb363355c4712ba6be0b260
ogawa0147 <m.ogawa0147@gmail.com>
2018-10-01 00:22:29 +0900

    [WIP] GIFカメラ

--- ExCamera/Scenes/CameraGif/CameraGifViewModel.swift
+++ ExCamera/Scenes/CameraGif/CameraGifViewModel.swift
@@ -1,19 +1,50 @@
 import Foundation
+import RxSwift
+import RxCocoa
 
-final class CameraGifViewModel {
-    private let navigator: CameraGifNavigator
-    let gifs: [GifImageSource]
-    let audios: [AudioSource]
-
-    init(navigator: CameraGifNavigator) {
-        let gifs = GifImageRepository().elements.map { GifImageSource(cgImages: $0.cgImages, delays: $0.delays, duration: $0.duration, defaultFrame: $0.defaultFrame, data: $0.data, animation: $0.animation, overlay: $0.overlay, scene: $0.scene) }
-        let audios = AudioRepository().elements.map { AudioSource(data: $0.data, fileURL: $0.fileURL) }
-        self.navigator = navigator
-        self.gifs = gifs
-        self.audios = audios
+final class CameraGifViewModel: ViewModelType {
+    struct Input {
+        let disposeBag: DisposeBag
+        let refreshTrigger: Driver<Void>
+//        let selectedURL: Driver<(url: URL, animated: Bool)>
+    }
+    struct Output {
+        let gifs: Driver<[GifImageSource]>
+        let audios: Driver<[AudioSource]>
     }
 
-    func toPlayer(url: URL, animated: Bool = true) {
-        navigator.toPlayer(url: url, animated: animated)
+    private let navigator: CameraGifNavigator
+
+    init(navigator: CameraGifNavigator) {
+        self.navigator = navigator
+    }
+
+    func transform(input: Input) -> Output {
+        let gifs = Observable.just(GifImageRepository().elements.map { GifImageSource(cgImages: $0.cgImages,
+                                                                                      delays: $0.delays,
+                                                                                      duration: $0.duration,
+                                                                                      defaultFrame: $0.defaultFrame,
+                                                                                      data: $0.data,
+                                                                                      animation: $0.animation,
+                                                                                      overlay: $0.overlay,
+                                                                                      scene: $0.scene) })
+        let audios = Observable.just(AudioRepository().elements.map { AudioSource(data: $0.data, fileURL: $0.fileURL) })
+
+        input.refreshTrigger.asObservable()
+            .withLatestFrom(gifs)
+            .subscribe()
+            .disposed(by: input.disposeBag)
+        input.refreshTrigger.asObservable()
+            .withLatestFrom(audios)
+            .subscribe()
+            .disposed(by: input.disposeBag)
+//        input.selectedURL.asObservable()
+//            .subscribe(onNext: { [weak self] in self?.navigator.toPlayer(url: $0.url, animated: $0.animated) })
+//            .disposed(by: input.disposeBag)
+
+        return Output(
+            gifs: gifs.asDriver(onErrorDriveWith: .empty()),
+            audios: audios.asDriver(onErrorDriveWith: .empty())
+        )
     }
 }
